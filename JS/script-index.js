cb897fb163f567c00331673fbdaa544ecb9f2d66
Breno Lucas <93356388+Breno2k@users.noreply.github.com>
2023-10-31 19:29:38 -0300

    criação da aba veterináro e animação no index

--- JS/script-index.js
+++ JS/script-index.js
@@ -1,9 +1,20 @@
-/* window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.segundaSeçaoMain', {
-    duration: 1000
+ScrollReveal().reveal('#caixa1', {
+    duration: 2500,
+    origin: '.paragrafoItalico',
+    rotate: { x: 80, y: 0, z: 0 },
+    distance: '200px',
 });
-
-sr.reveal('.terceiraSeçaoMain', {
-    duration: 3000
-}) */
+ScrollReveal().reveal('#caixa2', {
+    duration: 2750,
+    origin: '.paragrafoItalico',
+    rotate: { x: 80, y: 0, z: 0 },
+    distance: '200px' 
+});
+ScrollReveal().reveal('#caixa3', {
+    duration: 3000,
+    origin: '.paragrafoItalico',
+    rotate: { x: 80, y: 0, z: 0 },
+    distance: '200px' 
+});
